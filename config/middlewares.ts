6379c3991c8562a3a7aaab0d335d6da89d6835dd
fawwazmw <mahesfawwaz79@gmail.com>
2024-11-23 10:30:43 +0700

    first commit 3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,19 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:3000', // Frontend local development
+        'https://spotq.fwzdev.site', // Frontend production domain
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
